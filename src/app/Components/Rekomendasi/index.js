fa88c7a938649762e2a144e48c0acdd5d84e3d2a
kadekyana <kadekyana11@gmail.com>
2023-12-09 17:02:14 +0800

    First Web

---
+++ src/app/Components/Rekomendasi/index.js
@@ -0,0 +1,17 @@
+const Rekomendasi = () => {
+  return (
+    <div>
+      <h1>Wayang Terpopuler</h1>
+      <div className="grid grid-cols-3 gap-2 h-52">
+        <div className="bg-green-500">Card 1</div>
+        <div className="bg-green-500">Card 2</div>
+        <div className="bg-green-500">Card 3</div>
+        <div className="bg-green-500">Card 4</div>
+        <div className="bg-green-500">Card 5</div>
+        <div className="bg-green-500">Card 6</div>
+      </div>
+    </div>
+  );
+};
+
+export default Rekomendasi;
