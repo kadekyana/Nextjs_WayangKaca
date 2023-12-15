008f07b24c19098a8f1016ebd3cd78167d56038b
kadekyana <kadekyana11@gmail.com>
2023-12-15 15:14:57 +0800

    fix ukuran gambar

--- src/app/Components/Rekomendasi/index.js
+++ src/app/Components/Rekomendasi/index.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 const Rekomendasi = ({ name, images, id, about }) => {
   return (
     <div className="md:mx-3 mx-1 border-2 border-slate-300 rounded-lg">
-      <Image src={images} width={300} height={300} alt="Picture of the character" />
+      <Image src={images} width={250} height={250} alt="Picture of the character" />
       <h3 className="font-bold md:text-xl sm:text-md text-sm p-3">{name}</h3>
       <p className="md:text-md sm:text-sd text-sm p-3 truncate">{about}</p>
       <button className="px-8 py-2 border-2 ml-2 mb-2 rounded-md bg-indigo-500 text-white font-bold">
-        <Link href={`Detail/${id}`}>Detail</Link>
+        <Link href={`Detail`}>Detail</Link>
       </button>
     </div>
   );
--- src/app/detail/page.js
+++ src/app/detail/page.js
@@ -1,29 +1,30 @@
-import Image from "next/image"
+import Image from "next/image";
 
-const Detail = () =>{
-    return (
-        <div className="h-screen flex justify-center items-center">
-            <div className=" flex bg-white p-4 border rounded shadow-md">
-                <div className="border m-7">
-                    <Image src="/OIP.jpg" alt="" width={250} height={250} className="rounded object-cover h-full w-80"/>
-                </div>
-                <div className="p-2 mx-5 ">
-                    <h1 className="text-center pb-5 text-2xl">Ramayana</h1>
-                    <div className=" h-60 w-80">
-                        <p className="text-justify text-xl p-1 border">deskripsi So I started to walk into the water. I won't lie to you boys, I was
-                            terrified. But I pressed on, and as I made my way past the breakers
-                            a strange calm came over me. I don't know if it was divine intervention
-                            or the kinship of all 
-                        </p>
-                    </div>
-                    <div className="border mt-4">
-                        <h1>kuantitas</h1>
-                    </div>
-                    <button type="submit" className="w-full mt-3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Masukkan ke Keranjang</button>
-                </div>
-            </div>
+const Detail = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className=" flex bg-white p-4 border rounded shadow-md">
+        <div className="flex justify-center items-center m-7">
+          <Image src="/OIP.jpg" alt="" width={250} height={250} className="rounded" />
         </div>
-    )
-}
+        <div className="p-2 mx-5 ">
+          <h1 className="text-center pb-5 text-2xl">Ramayana</h1>
+          <div className=" h-60 w-80">
+            <p className="text-justify text-xl p-1 border">
+              deskripsi So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the
+              kinship of all
+            </p>
+          </div>
+          <div className="border mt-4">
+            <h1>kuantitas</h1>
+          </div>
+          <button type="submit" className="w-full mt-3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+            Masukkan ke Keranjang
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
