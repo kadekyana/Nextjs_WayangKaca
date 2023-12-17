0c95322fe0307d529b92125d7e63b4a53a9163e4
kadekyana <kadekyana11@gmail.com>
2023-12-17 11:36:42 +0800

    Update fix Login Register

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -1,35 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import RegisterForm from "../Components/RegisterForm";
 
 const Register = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className=" flex bg-white p-4 border rounded shadow-md ">
-        <div className=" ">
+        <div className="pt-5">
           <Image src="/wayang2.jpeg" alt="" width={250} height={250} className="h-96 w-96 rounded p-2" />
         </div>
         <div className="p-2 ">
           <h1 className="text-center pb-5 text-2xl">Register</h1>
-          <form className="py-2 px-3 ">
-            <div className="mb-4 ">
-              <label htmlFor="username"></label>
-              <input type="text" id="username" placeholder="Email" className="border-2 border-current w-72 p-2 rounded" />
-            </div>
-            <div className="mb-4 ">
-              <label htmlFor="password"></label>
-              <input type="text" id="password" placeholder="Password" className="border-2 border-current w-72 p-2 rounded" />
-            </div>
-            <div className="mb-4 ">
-              <label htmlFor="password"></label>
-              <input type="text" id="password" placeholder="Masukkan Password Lagi" className="border-2 border-current w-72 p-2 rounded" />
-            </div>
-            <button type="submit" className="w-full mt-3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
-              <Link href="/Login">Register</Link>
-            </button>
-            <Link href="/Login" className="ml-auto mt-3 float-right text-sm">
-              Kembali ke Login
-            </Link>
-          </form>
+          <RegisterForm />
         </div>
       </div>
     </div>
